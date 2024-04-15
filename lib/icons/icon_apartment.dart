b6db58fec188c723138a16f900d894e8b3478151
daeyeong40 <daeyeong40@naver.com>
2024-04-15 20:41:41 +0900

    2024.04.15 add, home, star 버튼 아이콘_2

---
+++ lib/icons/icon_apartment.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(child: SizedBox(
+              width: 100,
+              height: 100,
+              child: ApartmentIcon(iConSize: 70,))),
+        ),
+      )
+  );
+}
+
+class ApartmentIcon extends StatelessWidget {
+  final double iConSize;
+  /// 아이콘의 크기 값을 변수로 받아와야 할 듯
+
+  const ApartmentIcon({required this.iConSize, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(0), color: Colors.indigo),
+        child: Icon(Icons.add_alarm_outlined, color: Colors.white, size: iConSize,)
+    );
+
+    //     floatingActionButton: FloatingActionButton(
+    //   onPressed: _incrementCounter,
+    //   tooltip: 'Increment',
+    //   child: const Icon(Icons.add),
+  }
+}
+
+
