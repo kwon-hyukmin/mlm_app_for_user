f94268929d10b8adb307b419779e37a202abcfa8
daeyeong <daeyeong40@naver.com>
2024-04-18 23:03:24 +0900

    2024.04.18 UserPickUp 화면 기초틀

---
+++ lib/screens/userPickup.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const UserPickUp());
+}
+
+class UserPickUp extends StatelessWidget {
+  const UserPickUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
