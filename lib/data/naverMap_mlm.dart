a2c7f734737abbc4e8be34b7096c68f06ef4b954
daeyeong <daeyeong40@naver.com>
2024-04-25 01:49:47 +0900

    2024.04.24 네이버맵 삽질....

--- lib/data/naverMap_mlm.dart
+++ lib/data/naverMap_mlm.dart
@@ -5,52 +5,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:geolocator/geolocator.dart';
 
+
+
 void main() async {
 
-  await _initialize();
+  await NaverMapMlmApp_Initialize().initialize();
+  await Location().getCurrentLocation();
 
-  runApp(const NaverMapApp());
+  runApp(const NaverMapMlmApp());
 }
 
 // 지도 초기화하기
-Future<void> _initialize() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NaverMapSdk.instance.initialize(
-      clientId: 'dza205s9mz',     // 클라이언트 ID 설정
-      onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
-  );
-  // await Location();
+
+
+class NaverMapMlmApp_Initialize{
+  Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await NaverMapSdk.instance.initialize(
+        clientId: 'dza205s9mz',     // 클라이언트 ID 설정
+        onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
+    );
+    // await Location();
+  }
+
+
 }
 
-class NaverMapApp extends StatelessWidget {
-  const NaverMapApp({Key? key});
+
+class NaverMapMlmApp extends StatelessWidget {
+  const NaverMapMlmApp({Key? key});
+
 
   @override
   Widget build(BuildContext context) {
     // NaverMapController 객체의 비동기 작업 완료를 나타내는 Completer 생성
     final Completer<NaverMapController> mapControllerCompleter = Completer();
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Text("네이버지도"),
-              TextButton(onPressed: () async {
-                Location location = Location();
-                await location.getCurrentLocation();
-                print('object');
-              }, child: Text("위도경도 호출")),
-            ],
-          ),
-        ),
-        body: Stack(
-          children: [
 
-            NaverMap(
+    return Container(
+            child: NaverMap(
               options: const NaverMapViewOptions(
                     initialCameraPosition: NCameraPosition(
-                        target: NLatLng(37.5036313, 127.0471425),
+                        target: NLatLng(0, 0),
                         zoom: 14,
                         bearing: 0,
                         tilt: 0
@@ -69,27 +65,17 @@ class NaverMapApp extends StatelessWidget {
                 log("onMapReady", name: "onMapReady");
               },
             ),
-          ],
-        ),
-      ),
-    );
+        );
   }
 }
 
 
-// class LocationClass extends LatLng {
-//   final double latitude;
-//   final double longitude;
-//
-//   const LocationClass({required this.latitude, required this.longitude}) : super(latitude, longitude);
-// }
 
 class Location {
   double latitude = 0;
   double longitude = 0;
 
   Future<void> getCurrentLocation() async {
-    print(11111);
     LocationPermission permission = await Geolocator.checkPermission();
     // print(permission);
     if (permission == LocationPermission.denied) {
