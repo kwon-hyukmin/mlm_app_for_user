c02a5c18ae377575e310d39a9389627415fc4b33
daeyeong40 <daeyeong40@naver.com>
2024-04-24 20:38:03 +0900

    2024.04.24

--- lib/data/naverMap_test.dart
+++ lib/data/naverMap_test.dart
@@ -3,10 +3,12 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
-import 'package:naver_map_plugin/naver_map_plugin.dart' show LatLng;
+import 'package:geolocator/geolocator.dart';
 
 void main() async {
+
   await _initialize();
+
   runApp(const NaverMapApp());
 }
 
@@ -17,6 +19,7 @@ Future<void> _initialize() async {
       clientId: 'dza205s9mz',     // 클라이언트 ID 설정
       onAuthFailed: (e) => log("네이버맵 인증오류 : $e", name: "onAuthFailed")
   );
+  // await Location();
 }
 
 class NaverMapApp extends StatelessWidget {
@@ -29,21 +32,44 @@ class NaverMapApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        body: NaverMap(
-          options: const NaverMapViewOptions(
-            extent: NLatLngBounds(
-              southWest: NLatLng(31.43, 122.37),
-              northEast: NLatLng(44.35, 132.0),
-            ),
-            locale: Locale('ko'),
-            indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
-            locationButtonEnable: true,    // 위치 버튼 표시 여부 설정
-            consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Text("네이버지도"),
+              TextButton(onPressed: () async {
+                Location location = Location();
+                await location.getCurrentLocation();
+                print('object');
+              }, child: Text("위도경도 호출")),
+            ],
           ),
-          onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
-            mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
-            log("onMapReady", name: "onMapReady");
-          },
+        ),
+        body: Stack(
+          children: [
+
+            NaverMap(
+              options: const NaverMapViewOptions(
+                    initialCameraPosition: NCameraPosition(
+                        target: NLatLng(37.5036313, 127.0471425),
+                        zoom: 14,
+                        bearing: 0,
+                        tilt: 0
+                    ),
+                extent: NLatLngBounds(
+                  southWest: NLatLng(31.43, 122.37),
+                  northEast: NLatLng(44.35, 132.0),
+                ),
+                locale: Locale('ko'),
+                indoorEnable: true,             // 실내 맵 사용 가능 여부 설정
+                locationButtonEnable: true,    // 위치 버튼 표시 여부 설정
+                consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
+              ),
+              onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
+                mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
+                log("onMapReady", name: "onMapReady");
+              },
+            ),
+          ],
         ),
       ),
     );
@@ -51,9 +77,34 @@ class NaverMapApp extends StatelessWidget {
 }
 
 
-class LocationClass extends LatLng {
-  final double latitude;
-  final double longitude;
+// class LocationClass extends LatLng {
+//   final double latitude;
+//   final double longitude;
+//
+//   const LocationClass({required this.latitude, required this.longitude}) : super(latitude, longitude);
+// }
 
-  const LocationClass({required this.latitude, required this.longitude}) : super(latitude, longitude);
+class Location {
+  double latitude = 0;
+  double longitude = 0;
+
+  Future<void> getCurrentLocation() async {
+    print(11111);
+    LocationPermission permission = await Geolocator.checkPermission();
+    // print(permission);
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      latitude = position.latitude;
+      longitude = position.longitude;
+      print(latitude);
+      print(longitude);
+    } catch (e) {
+      print(e);
+    }
+
+  }
 }
